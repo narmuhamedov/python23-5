d45b7c55c64ecbca6257293934e91e139a04a427
RadomirRZ <kingradomir@gmail.com>
2023-02-11 17:56:06 +0600

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('hello_link/', views.hello_view, name='hello'),
+    path('blog_link', views.blog_view, name='blog'),
+]
