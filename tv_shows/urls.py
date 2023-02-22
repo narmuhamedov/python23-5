14cb920d4e42ccd1ba5e037c56feeba73efb0ddf
RadomirRZ <kingradomir@gmail.com>
2023-02-22 17:14:44 +0600

    'lesson4'

--- tv_shows/urls.py
+++ tv_shows/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('tvshow/', views.tv_showview, name='show'),
     path('tvshow/<int:id>/', views.tv_show_detailview, name='details'),
+    path('tvshow/<int:id>/update/', views.update_tv_show_view, name='update'),
+    path('tvshow/<int:id>/delete/', views.delete_tv_show_view, name='delete'),
+    path('add-tv/', views.create_tv_show_view, name='create'),
 ]
--- tv_shows/views.py
+++ tv_shows/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from . import models
+from . import models, forms
 
 
 #вывод не полной информации
@@ -11,3 +12,43 @@ def tv_showview(request):
 def tv_show_detailview(request, id):
     show_id = get_object_or_404(models.TVShow, id=id)
     return render(request, 'tvshow_detail.html', {'show_id': show_id})
+
+
+
+#Добавление фильма через формы
+def create_tv_show_view(request):
+    method = request.method
+    if method == 'POST':
+        form = forms.TvShowForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('<h2>Фильм успешно добавлен!!!</h2>')
+
+    else:
+        form = forms.TvShowForm()
+
+    return render(request, 'add_tvshow.html', {'form': form})
+
+
+#изменение данных о фильме
+def update_tv_show_view(request, id):
+    show_object = get_object_or_404(models.TVShow, id=id)
+    if request.method == 'POST':
+        form = forms.TvShowForm(instance=show_object, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('<h2>Фильм успешно обновлен!</h2>')
+    else:
+        form = forms.TvShowForm(instance=show_object)
+
+    return render(request, 'update_tv_show.html', {
+                                                    'form': form,
+                                                    'object': show_object
+                                                   })
+
+
+#Удаление из базы
+def delete_tv_show_view(request, id):
+    show_object = get_object_or_404(models.TVShow, id=id)
+    show_object.delete()
+    return HttpResponse('<h2>Фильм успешно удален</h2>')
